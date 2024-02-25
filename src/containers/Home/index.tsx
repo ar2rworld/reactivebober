952f1cbdbdd905682bcc91fd745d3666be4c5ed7
Artur Linnik <ar2r.world@gmail.com>
2024-02-24 22:24:22 -0500

    add useEffect

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Game2 from "../../components/Game";
 import Info from "../../components/Game/Info";
 import "./index.css";
 
 function Home(){
   const [name, setName] = useState("");
+  const [showHelloMessage, setShowHelloMessage] = useState(false);
+
+  let  helloMessage = "Hello from friendly ReactiveBober!";
+  useEffect(()=>{
+    setTimeout(()=> {
+      setShowHelloMessage(true);
+    }, 2000)
+  })
 
   return (
     <div className="home">
-        <Game2 name={name}/>
-        <Info setName={setName} />
+      <Game2 name={name} />
+      <Info setName={setName} />
+      {showHelloMessage ? <p className="hello">{helloMessage}</p> : ""}
     </div>
   );
 }
